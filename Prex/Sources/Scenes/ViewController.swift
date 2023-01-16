34137cb2c459cea6e0f0e7333add219ee46d3cba
Dmitry Savelev <dsavelev@sportmasterlab.net>
2023-01-16 08:50:13 +0300

    Start Example

---
+++ Prex/Sources/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Prex
+//
+//  Created by Savelev Dmitriy on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .cyan
+	}
+}
+
